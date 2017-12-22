6173214c1afb17b01bbb6db23ac30179596bb270
Andrew Clinton <ddrew.clinton@gmail.com>
2017-12-22 15:07:40 -0500

    Created the properties service

---
+++ src/services/PropertiesService.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace anecka\retsrabbit\services;
+
+use anecka\retsrabbit\RetsRabbit;
+use Craft;
+use craft\base\Component;
+use RetsRabbit\ApiService;
+use RetsRabbit\Bridges\CraftBridge;
+use RetsRabbit\Resources\PropertiesResource;
+
+class PropertiesService extends Component
+{
+	/**
+	 * The api service from the core RR library
+	 * 
+	 * @var ApiService
+	 */
+	private $api;
+
+	/**
+	 * The properties resource endpoint
+	 * 
+	 * @var PropertiesResource
+	 */
+	private $resource;
+
+	/**
+	 * Constructor
+	 */
+	public function __construct()
+	{
+		$settings = RetsRabbit::$plugin->getSettings();
+		$bridge = new CraftBridge;
+
+		//Set the token fetcher function so the core lib can grab tokens
+		//from cache on the plugin's behalf
+		$bridge->setTokenFetcher(function () {
+			return RetsRabbit::$plugin->cache->get('access_token', true);
+		});
+
+		//Load the Craft Bridge into the ApiService
+		$this->api = new ApiService($bridge);
+
+		//Allow developer to override base endpoint
+		if($settings->apiEndpoint) {
+			$this->api->overrideBaseApiEndpoint($settings->apiEndpoint);
+		}
+
+		//Instantiate the PropertiesResource
+		$this->resource = new PropertiesResource($this->api);
+	}
+
+	/**
+	 * @param  array
+	 * @return array
+	 */
+	public function search($params = array())
+	{
+		$res = $this->resource->search($params);
+
+		if($res->didFail()) {
+			$contents = $res->getResponse();
+
+			if(isset($contents['error']) && isset($contents['error']['code'])) {
+				RetsRabbitPlugin::log('A permission error occurred.', LogLevel::Error);
+				
+				$code = $contents['error']['code'];
+
+				if($code == 'permission') {
+					$success = RetsRabbit::$plugin->tokens->refresh();
+
+					if(!is_null($success)) {
+						$res = $this->resource->search($params);
+					} else {
+						RetsRabbitPlugin::log('Could not refresh the token during a search.', LogLevel::Error);
+					}
+				}
+			}
+		}
+
+		return $res;
+	}
+
+	/**
+	 * @param  string
+	 * @return array
+	 */
+	public function find($id = '', $params = array())
+	{
+		$res = $this->resource->single($id, $params);
+
+		if($res->didFail()) {
+			$contents = $res->getResponse();
+
+			if(isset($contents['error']) && isset($contents['error']['code'])) {
+				RetsRabbitPlugin::log('A permission error occurred.', LogLevel::Error);
+				
+				$code = $contents['error']['code'];
+
+				if($code == 'permission') {
+					$success = RetsRabbit::$plugin->tokens->refresh();
+
+					if(!is_null($success)) {
+						$res = $this->resource->single($id, $params);
+					} else {
+						RetsRabbitPlugin::log('Could not refresh the token during property lookup.', LogLevel::Error);
+					}
+				}
+			}
+		}
+
+		return $res;
+	}
+}
