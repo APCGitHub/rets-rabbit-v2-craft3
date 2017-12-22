fceefd3b7f5904946094fd536d07af4625c8f7ce
Andrew Clinton <ddrew.clinton@gmail.com>
2017-12-22 15:06:20 -0500

    Created the searches record

---
+++ src/records/SearchesRecord.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace anecka\retsrabbit\records;
+
+use anecka\retsrabbit\RetsRabbit;
+
+use Craft;
+use craft\db\ActiveRecord;
+
+class SearchesRecord
+{
+	/**
+	 * @return string
+	 */
+	public static function tableName()
+	{
+		return 'retsrabbit_searches';
+	}
+
+	/**
+	 * @return array
+	 */
+	public function defineAttributes()
+	{
+		return array(
+			'type' => array(
+				AttributeType::String, 
+				'required' => true, 
+				'default' => 'property'
+			),
+			'params' => array(
+				AttributeType::String, 
+				'column' => ColumnType::Text, 
+				'required' => true
+			)
+		);
+	}
+}
