3f2be5842cdb30ca43b99bcf1f985faf10364c74
Andrew Clinton <ddrew.clinton@gmail.com>
2018-11-13 22:05:03 -0500

    Update to craft syntax

--- src/records/SearchRecord.php
+++ src/records/SearchRecord.php
@@ -2,37 +2,15 @@
 
 namespace anecka\retsrabbit\records;
 
-use Craft;
-
-use anecka\retsrabbit\RetsRabbit;
 use craft\db\ActiveRecord;
 
 class SearchRecord extends ActiveRecord
 {
-	/**
-	 * @return string
-	 */
-	public static function tableName()
-	{
-		return '{{%rets_rabbit_searches}}';
-	}
-
-	/**
-	 * @return array
-	 */
-	public function defineAttributes()
-	{
-		return array(
-			'type' => array(
-				AttributeType::String, 
-				'required' => true, 
-				'default' => 'property'
-			),
-			'params' => array(
-				AttributeType::String, 
-				'column' => ColumnType::Text, 
-				'required' => true
-			)
-		);
-	}
+    /**
+     * @return string
+     */
+    public static function tableName(): string
+    {
+        return '{{%rets_rabbit_searches}}';
+    }
 }
