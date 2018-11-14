3e9d4fee64a216833a15c4da256b73c1be0ba700
Andrew Clinton <ddrew.clinton@gmail.com>
2018-11-13 22:06:07 -0500

    Syntax updates

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -27,7 +27,7 @@ class Settings extends Model
 	/**
 	 * @return array
 	 */
-	public function rules()
+	public function rules(): array
 	{
 		return [
 			[['clientId', 'clientSecret'], 'required']
