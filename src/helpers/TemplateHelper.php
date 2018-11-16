8b97721c986a6739eb7b42ddb6fa2307269735e4
Andrew Clinton <ddrew.clinton@gmail.com>
2018-11-15 22:41:06 -0500

    Update to use view model and newest paginate var

--- src/helpers/TemplateHelper.php
+++ src/helpers/TemplateHelper.php
@@ -2,135 +2,124 @@
 
 namespace apc\retsrabbit\helpers;
 
+use apc\retsrabbit\converters\ListingConverter;
+use apc\retsrabbit\ViewModel;
 use Craft;
-use League\Fractal\Manager;
-use League\Fractal\Resource\Item;
-use League\Fractal\Resource\Collection;
+use craft\web\twig\variables\Paginate;
 
 use apc\retsrabbit\models\SearchCriteriaModel;
 use apc\retsrabbit\RetsRabbit;
-use apc\retsrabbit\serializers\RetsRabbitArraySerializer;
-use apc\retsrabbit\transformers\PropertyTransformer;
+use Exception;
 
 class TemplateHelper
 {
     /**
      * @param SearchCriteriaModel $criteria
      * @return array
-     * @throws \Exception
+     * @throws Exception
      */
-	public static function paginateProperties(SearchCriteriaModel $criteria)
-	{
-		if($criteria->getSearchId() === null) {
-			throw new \Exception('The search id was not supplied.');
-		}
+    public static function paginateProperties(SearchCriteriaModel $criteria): array
+    {
+        if ($criteria->getSearchId() === null) {
+            throw new Exception('The search id was not supplied.');
+        }
 
-		$paginateV = new PaginateVariable;
-		$currentPage = Craft::$app->request->getPageNum();
-		$search = RetsRabbit::$plugin->searches->getById($criteria->getSearchId());
-		$viewData = null;
-		$fractal = new Manager();
-		$fractal->setSerializer(new RetsRabbitArraySerializer);
+        $paginateV   = new Paginate();
+        $currentPage = Craft::$app->request->getPageNum();
+        $search      = RetsRabbit::$plugin->getSearches()->getById($criteria->getSearchId());
+        $viewModel   = new ViewModel();
 
-		if($search) {
-			$savedSearchParams = json_decode($search->params, true);
+        if ($search) {
+            $savedSearchParams = json_decode($search->params, true);
 
-			//Count total related variables
-			$countParams = array_merge($savedSearchParams, array('$select' => $criteria->countMethod));
-			$countCacheKey = 'pagination/' . hash('sha256', serialize($countParams));
-			$total = RetsRabbit::$plugin->cache->get($countCacheKey);
-			$perPage = $criteria->getPerPage();
-			$countError = false;
+            //Count total related variables
+            $countParams   = array_merge($savedSearchParams, ['$select' => $criteria->countMethod]);
+            $countCacheKey = 'pagination/' . hash('sha256', serialize($countParams));
+            $total         = RetsRabbit::$plugin->getCache()->get($countCacheKey);
+            $perPage       = $criteria->getPerPage();
+            $countError    = false;
 
-			//Search related variables
-			$mergeableKeys = array('$select', '$orderby');
-			$queryParams = $savedSearchParams;
-			$queryParams['$top'] = $perPage;
-			$overrides = $criteria->get();
-			$data = array();
-			$searchError = false;
+            //Search related variables
+            $mergeableKeys       = ['$select', '$orderby'];
+            $queryParams         = $savedSearchParams;
+            $queryParams['$top'] = $perPage;
+            $overrides           = $criteria->get();
 
-			foreach($mergeableKeys as $key) {
-				if(isset($overrides[$key])) {
-					$queryParams[$key] = $overrides[$key];
-				}
-			}
+            foreach ($mergeableKeys as $key) {
+                if (isset($overrides[$key])) {
+                    $queryParams[$key] = $overrides[$key];
+                }
+            }
 
-			if($currentPage > 1) {
-				$queryParams['$skip'] = ($currentPage - 1) * $queryParams['$top'];
-			}
+            if ($currentPage > 1) {
+                $queryParams['$skip'] = ($currentPage - 1) * $queryParams['$top'];
+            }
 
-			$searchCacheKey = 'searches/' . hash('sha256', serialize($queryParams));
-			$data = RetsRabbit::$plugin->cache->get($searchCacheKey);
-			
+            $searchCacheKey = 'searches/' . hash('sha256', serialize($queryParams));
+            $listingData    = RetsRabbit::$plugin->getCache()->get($searchCacheKey);
 
-			//Try to fetch the search total
-			if($total === null || $total === FALSE) {
-				$res = RetsRabbit::$plugin->properties->search($countParams);
+            if ($total === false) {
+                $res = RetsRabbit::$plugin->getProperties()->search($countParams);
 
-				if(!$res->didSucceed()) {
-					$countError = true;
-				} else {
-					$total = $res->getResponse()['@retsrabbit.total_results'];
+                if ($res->didSucceed()) {
+                    $total = $res->getResponse()['@retsrabbit.total_results'];
 
-					RetsRabbit::$plugin->cache->set($countCacheKey, $total, 3600);
-				}
-			}
+                    RetsRabbit::$plugin->getCache()->set($countCacheKey, $total, 3600);
+                } else {
+                    $countError = true;
+                }
+            }
 
-			//Try to fetch the search results
-			if(is_null($data) || empty($data)) {
-				$res = RetsRabbit::$plugin->properties->search($queryParams);
+            //Try to fetch the search results
+            if ($listingData === false) {
+                $res = RetsRabbit::$plugin->getProperties()->search($queryParams);
 
-				if(!$res->didSucceed()) {
-					$searchError = true;
-				} else {
-					$data = $res->getResponse()['value'];
+                if (!$res->didSucceed()) {
+                    $viewModel->errors = RetsRabbit::$plugin->getApiResponses()->getResponseErrors($res);
+                } else {
+                    $viewModel->data = (new ListingConverter())->parseCollection(
+                        $res->getResponse()['value'] ?? [],
+                        new ListingConverter()
+                    );
 
-					RetsRabbit::$plugin->cache->set($searchCacheKey, $data, 3600);
-				}
-			}
+                    RetsRabbit::$plugin->getCache()->set($searchCacheKey, $viewModel, 3600);
+                }
+            } else {
+                $viewModel = $listingData;
+            }
 
-			if(!$searchError) {
-				if(empty($data)) {
-					$viewData = array();
-				} else {
-					$resources = new Collection($data, new PropertyTransformer);
-	        		$viewData = $fractal->createData($resources)->toArray();
-				}
-			}
+            if ($countError || $viewModel->hasErrors() || $total == 0) {
+                return [$paginateV, $viewModel];
+            }
 
-			if($countError || $searchError || $total == 0) {
-				return array($paginateV, $viewData);
-			}
+            if (!$perPage) {
+                $perPage = $total;
+            }
 
-			if(!$perPage) {
-				$perPage = $total;
-			}
+            $totalPages = ceil($total / $perPage);
 
-			$totalPages = ceil($total / $perPage);
+            if ($totalPages == 0) {
+                return [$paginateV, $viewModel];
+            }
 
-			if($totalPages == 0) {
-				return array($paginateV, $viewData);
-			}
+            if ($currentPage > $totalPages) {
+                $currentPage = $totalPages;
+            }
 
-			if ($currentPage > $totalPages) {
-				$currentPage = $totalPages;
-			}
+            $offset = $perPage * ($currentPage - 1);
+            $last   = $offset + $perPage;
 
-			$offset = $perPage * ($currentPage - 1);
-			$last = $offset + $perPage;
+            if ($last > $total) {
+                $last = $total;
+            }
 
-			if($last > $total) {
-				$last = $total;
-			}
+            $paginateV->first       = $offset + 1;
+            $paginateV->last        = $last;
+            $paginateV->total       = $total;
+            $paginateV->currentPage = $currentPage;
+            $paginateV->totalPages  = $totalPages;
+        }
 
-			$paginateV->first = $offset + 1;
-			$paginateV->last = $last;
-			$paginateV->total = $total;
-			$paginateV->currentPage = $currentPage;
-			$paginateV->totalPages = $totalPages;
-		}
-
-		return array($paginateV, $viewData);
-	}
+        return [$paginateV, $viewModel];
+    }
 }
